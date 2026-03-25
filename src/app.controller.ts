efade5e3c8468a118fb14bab74626839fdb807a4
event <event@z2t13c2.42bangkok.com>
2026-03-25 11:06:55 +0700

    p01

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -8,7 +8,7 @@ export class AppController {
 
   @Get()
   getHello(): string {
-    const result = this.appService.getHello();
+    //const result = this.appService.getHello();
     return this.appService.getHello();
   }
 }
