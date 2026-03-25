166f24d3fa8bb97aaade83458e28bf22c85b2290
event <event@z2t13c2.42bangkok.com>
2026-03-25 11:38:30 +0700

    p01

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,6 +1,13 @@
 import { Injectable } from '@nestjs/common';
 import { ApiResponse } from './interfaces/response.interface';
-
+// Object {
+//       "data": Object {
+//       "service": "purchase-api",
+//   -     "version": "1.0.0",
+//   -   },
+//   -   "message": "Hello NestJS",
+//   -   "success": true,
+//   - }
 @Injectable()
 export class AppService {
   getHello(): string {
